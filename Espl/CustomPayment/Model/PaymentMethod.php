69821856a8ef0579f5f591dfe42046a41c028139
apurva-shepal <apurva.shepal@eternussolutions.com>
2020-11-09 15:19:47 +0530

    Payment gateway

---
+++ Espl/CustomPayment/Model/PaymentMethod.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Espl\CustomPayment\Model;
+
+class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod {
+
+    protected $_code = 'custompayment';
+
+}
---
+++ Espl/CustomPayment/view/frontend/web/js/view/payment/method-renderer.js
@@ -0,0 +1,20 @@
+
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
+        'use strict';
+        rendererList.push(
+            {
+                type: 'custompayment',
+                component: 'Espl_CustomPayment/js/view/payment/method-renderer/custompayment'
+            }
+        );
+        return Component.extend({});
+    }
+);
